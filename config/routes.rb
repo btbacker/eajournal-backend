fcfc38b9d55bbdfddb29886dec98f9fde38508aa
btbacker <33066436+btbacker@users.noreply.github.com>
2020-12-11 10:01:14 -0800

    fixed auth

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, :defaults => { :format => 'json' }
       post '/login', to: 'auth#login'
       get '/profile', to: 'users#profile'
     end
   end
+  # post '/login', to: 'auth#login'
+  # get '/profile', to: 'users#profile'
 end
